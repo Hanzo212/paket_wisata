ed33f7fbb17220f81f88ca7ea9ca653cf2e190ae
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-11 15:31:46 +0700

    test

---
+++ src/test/java/com/project/paket_wisata/PwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.paket_wisata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
