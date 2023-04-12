3fd4f831862e9c8774695502633e742b0e974d68
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-12 08:41:30 +0700

    cakra

---
+++ src/main/java/com/project/paket_wisata/model/paket.java
@@ -0,0 +1,19 @@
+package com.project.paket_wisata.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class paket {
+	
+	@Id
+	@Column (name="nama_paket")
+	private String nama_paket;
+	@Column(name="harga_paket")
+	private Double harga_paket;
+}
---
+++ src/main/java/com/project/paket_wisata/model/pelanggan.java
@@ -0,0 +1,24 @@
+package com.project.paket_wisata.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class pelanggan {
+	
+	@Id
+	@Column(name="id_pelanggan")
+	private int id_pelanggan;
+	@Column(name="nama_pelanggan")
+	private String nama_pelanggan;
+	@Column(name="email_pelanggan")
+	private String email_pelanggan;
+	@Column(name="no_rek")
+	private int no_rek;
+
+}
